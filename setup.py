e9510584d89866f0d5a0c2c41a7c749a6c82d163
Alizee Pace <alizee.pace@gmail.com>
2016-08-24 10:54:07 +0200

    installation: Flask>=0.11.1
    * Removed dependency on Flask-CLI.
    
    Signed-off-by: Alizee Pace <alizee.pace@gmail.com>

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ readme = open('README.rst').read()
 history = open('CHANGES.rst').read()
 
 requirements = [
-    'Flask>=0.10.1',
+    'Flask>=0.11.1',
     'backports.lzma>=0.0.3',
     'invenio-base>=0.3.0',
     'six>=1.7.2',
